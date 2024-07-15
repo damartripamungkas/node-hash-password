a1233979effcecb170ee664b511b45016f25c434
damartripamungkas <damartripamungkas@github.com>
2024-07-15 15:09:16 +0700

    feat: update core file

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,48 @@
-import { utils } from "ethers";
+import { keccak256, sha256, sha512, toUtf8Bytes } from "ethers"
+import { TypeCompare } from "./type"
 
-interface IfacePassword {
-  password: string;
-}
-
-interface IfaceCompare {
-  method: string;
-  hash: string;
-  password: string;
-}
+const to8bytes = (v: any) => toUtf8Bytes(v.toString())
+const strKeccak256 = `keccak256`
+const strSha256 = `sha256`
+const strSha512 = `sha512`
 
 /**
  *
- * @param_password string plain text
- * @returns string hash with length 66
+ * @password string plain text
+ * @returns string hash with prefix 0x
  */
-const hashWithKeccak256 = (param: IfacePassword) => {
-  const { password } = param;
-  return utils.keccak256(utils.toUtf8Bytes(password.toString()));
-};
+const hashWithKeccak256 = (password: string) => keccak256(to8bytes(password))
 
 /**
  *
- * @param_password string plain text
- * @returns string hash with length 66
+ * @password string plain text
+ * @returns string hash with prefix 0x
  */
-const hashWithSha256 = (param: IfacePassword) => {
-  const { password } = param;
-  return utils.sha256(utils.toUtf8Bytes(password.toString()));
-};
+const hashWithSha256 = (password: string) => sha256(to8bytes(password))
 
 /**
  *
- * @param_password string plain text
- * @returns string hash with length 130
+ * @password string plain text
+ * @returns string hash with prefix 0x
  */
-const hashWithSha512 = (param: IfacePassword) => {
-  const { password } = param;
-  return utils.sha512(utils.toUtf8Bytes(password.toString()));
-};
+const hashWithSha512 = (password: string) => sha512(to8bytes(password))
 
 /**
  *
- * @param_method string. algoritma method example keccak256, sha256, sha512
- * @param_hash string. result from algoritma hash
- * @param_password string. your pure password plain text
+ * @method string. algoritma method example keccak256, sha256, sha512
+ * @hash string. result from algoritma hash
+ * @password string. your pure password plain text
  * @returns boolean. if true password and hash is match
  */
-const compareHashAndPassword = (param: IfaceCompare) => {
-  const { method, hash, password } = param;
-  const hashFunc = method == "keccak256" ? hashWithKeccak256 : method == "sha256" ? hashWithSha256 : method == "sha512" ? hashWithSha512 : null;
-  if (hashFunc === null) throw new Error("method is not found");
-  const result = hash.toLowerCase() == hashFunc({ password });
-  return result;
-};
+const compareHashAndPassword = (param: TypeCompare) => {
+  const { method, hash, password } = param
+  const hashFunc = method == strKeccak256 ? hashWithKeccak256 : method == strSha256 ? hashWithSha256 : method == strSha512 ? hashWithSha512 : null
+  if (hashFunc === null) {
+    throw new Error(`method is not found`)
+  }
+  const result = hash.toLowerCase() == hashFunc(password)
+  return result
+}
 
-export { hashWithKeccak256, hashWithSha256, hashWithSha512, compareHashAndPassword };
-export default { hashWithKeccak256, hashWithSha256, hashWithSha512, compareHashAndPassword };
+export { to8bytes, hashWithKeccak256, hashWithSha256, hashWithSha512, compareHashAndPassword, strKeccak256, strSha256, strSha512 }
+export default { to8bytes, hashWithKeccak256, hashWithSha256, hashWithSha512, compareHashAndPassword, strKeccak256, strSha256, strSha512 }
